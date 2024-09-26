c7c31d1d612ca682e78a08463d87ed3cc899c24d
alexander.churkin <a.churkin@osite.de>
2024-09-26 15:48:41 +0200

    updated Code to function better
    yes

--- ESP_Flash/ESP32_Config_Proxsens0.cpp
+++ ESP_Flash/ESP32_Config_Proxsens0.cpp
@@ -3,17 +3,19 @@ First set of 2 sensors sending a positive signal to main controller if read valu
 Const Short Numbers have to be changed before running according to the ESP controller used and change short to int if sensor output too high
 */
 
-const short rangeL = 2; //input for range
-const short rangeR = 5; //input for range
-const short output0 = 16; //TX_0
-const short output1 = 1; //TX_1
-const short input = 6; //RX_0
+const short input_range_L = 2; //input for range
+const short input_range_R = 5; //input for range
+const short Tx_main0 = 16; //TX_0
+const short Rx_main0 = 6; //RX_0
 
-short signal = 0;
+const bool F = false;
+const bool T = true;
+
 unsigned short ragne_L_0 = 0;
 unsigned short range_R_0 = 0;
-bool i0 = false;
-bool i1 = false;
+bool i0 = F;
+bool accept0 = F;
+bool accept1 = F;
 
 const unsigned short max = 20; //the maximum range counted as true (needs to be calibrated if == 20)
 //const unsigned short max1 = 10;
@@ -21,46 +23,69 @@ const unsigned short max = 20; //the maximum range counted as true (needs to be 
 
 int setup(){
 	
-	pinMode(rangeL,INPUT);
-	pinMode(rangeR,INPUT);
-	pinMode(output0,OUTPUT);
-	pinMode(output1,OUTPUT);
-	pinMode(input,INPUT);
+	pinMode(input_range_L,INPUT);
+	pinMode(input_range_R,INPUT);
+	pinMode(Tx_main0,OUTPUT);
+	pinMode(Rx_main0,INPUT);
 
 	//create a task that will be executed in the Task2code() function, with priority 1 and executed on core 0
-    xTaskCreatePinnedToCore(Task1code, "Task1", 10000, NULL, 1, &Task1, 0);                       
+    xTaskCreatePinnedToCore(Task1code, "Task0", 10000, NULL, 1, &Task0, 0);                       
     delay(500); 
 
   	//create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
-  	xTaskCreatePinnedToCore(Task2code, "Task2", 10000, NULL, 1, &Task2, 1);           
+  	xTaskCreatePinnedToCore(Task2code, "Task1", 10000, NULL, 1, &Task1, 1);           
     delay(500); 
 }
-void Task1code(void * pvParameters){ //check Left faar sensor and locks itself if something has been spotted calling for other conntoller to verify, if verification fails returns back to the main programm
-	ragne_L_0 = analogRead(rangeL);
-	if (ragne_L_0 < max){
-		digitalWrite(output0, HIGH); //sends signal to the main Controller initiating other sensors
-		ragne_L_0 = NULL;
-		for (i0, true, ){
-			i0 = DigitalRead(input) == HIGH ? true : false;
+// constantly reads values
+void Task0code(void * pvParameters){
+	for(;;)
+		while (stop_range == F){
+			ragne_L = analogRead(input_range_L);
+			accept0 = (ragne_L_0 < max) ? T : F;
+			ragne_R = analogRead(input_range_R);
+			accept1 = (ragne_R_0 < max) ? T : F;
 		}
-	}
-	else {
-		digitalWrite(output0, LOW);
-	}
-	digitalWrite(output0, LOW);
+
+		while (stop_range == T){
+			i0 = (DigitalRead(Rx_main0) == HIGH) ? T : F;
+			if(i0 == T){
+				stop_range = F;
+			}
+		}
 }
-void Task2code(void * pvParameters){ //check Right faar sensor and locks itself if something has been spotted calling for other conntoller to verify, if verification fails returns back to the main programm
-	range_R_0 = analogRead(rangeR);
-	Calculate(ragne_R_0);
-	if (ragne_R_0 < max){
-		digitalWrite(output1, HIGH); //sends signal to the main Controller initiating other sensors
-		range_R_0 = NULL;
-		for (i1, true, ){ //loops untill the initiated other Controller Denies the verification
-			i1 = DigitalRead(input) == HIGH ? true : false;
+//check Right faar sensor and locks itself if something has been spotted calling for other conntoller to verify, if verification fails returns back to the main programm
+void Task1code(void * pvParameters){
+	for(;;){
+		switch(accept0){
+			case T:
+				DigitalWrite(Tx_main0, HIGH);
+				stop_range = T;
+				
+				for (i0 = F; T; ){
+				}
+
+				DigitalWrite(Tx_main0, LOW);
+				accept0 = F;
+				break;
+
+			default:
+				switch(accept1){
+					case true:
+						DigitalWrite(Tx_main0, HIGH);
+						stop_range = T;
+
+						for (i0 = F; T; ){
+						}
+
+						DigitalWrite(Tx_main0, LOW);
+						accept1 = F;
+						break;
+
+					default:
+						DigitalWrite(Tx_main0, LOW);
+						break;
+				}
+				break;
 		}
 	}
-	else {
-		digitalWrite(output1, LOW);
-	}
-	digitalWrite(output1, LOW);
 }
